e5eae877fbb2c232699f8bf8fd9b531cc7fe208e
Corintho Assuncao <corintho.rodrigues.de.assuncao@nn-group.com>
2023-09-28 13:04:04 +0200

    chore: remove keymap that already has a default mapping in LazyVim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,12 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps hereby
-vim.keymap.set(
-  "n",
-  "<leader>sx",
-  require("telescope.builtin").resume,
-  { noremap = true, silent = true, desc = "Resume" }
-)
 vim.keymap.set("n", "<leader>gm", function()
   require("neogit").open({ kind = "auto" })
 end, { desc = "magit" })
