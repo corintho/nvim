37f20b4af9c6def504c7b0881d71355d77f83a41
Corintho Assuncao <corintho.rodrigues.de.assuncao@nn-group.com>
2023-10-02 10:03:25 +0200

    feat: add lualine status for codeium

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,9 +1,22 @@
 return {
-  "Exafunction/codeium.vim",
-  lazy = false,
-  config = function()
-    vim.keymap.set("i", "<M-g>", function()
-      return vim.fn["codeium#Accept"]()
-    end, { expr = true })
-  end,
+  {
+    "Exafunction/codeium.vim",
+    lazy = false,
+    config = function()
+      vim.keymap.set("i", "<M-g>", function()
+        return vim.fn["codeium#Accept"]()
+      end, { expr = true })
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      table.insert(opts.sections.lualine_x, {
+        function()
+          return vim.fn["codeium#GetStatusString"]()
+        end,
+      })
+    end,
+  },
 }
